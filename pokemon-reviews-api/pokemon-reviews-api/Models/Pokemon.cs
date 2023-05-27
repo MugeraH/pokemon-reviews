c0ae70e3cc2ac79af8337a8027e74a1c84d1d1f6
unknown <Administrator@HDQ44619.kq.kenya-airways.com>
2023-05-27 16:11:09 +0300

    creating many to many relationship and join tables

--- pokemon-reviews-api/pokemon-reviews-api/Models/Pokemon.cs
+++ pokemon-reviews-api/pokemon-reviews-api/Models/Pokemon.cs
@@ -6,7 +6,9 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime BirthDate { get; set; }
-
         public ICollection<Review> Reviews { get; set;}
+
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategory { get; set; }
     }
 }
